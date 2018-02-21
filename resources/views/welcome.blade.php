79638192ab5672ca29c8eb946cadfd05cd181338
soukrat <32969561+soukrat@users.noreply.github.com>
2018-02-21 15:13:38 +0000

    css
    sss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: red;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
